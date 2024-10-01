b937a06e0fe2eab7de3a8d98ee9e9963f231bf6b
Luchenchen1 <145833354+Luchenchen1@users.noreply.github.com>
2024-10-01 20:40:27 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,14 +1,10 @@
-//这是数据库的配置文件
+// This is the configuration file for the database
 module.exports = {
-	// 主机名  数据库所在电脑的主机名
+	// Host Name Host name of the computer where the database is located
 	host:'localhost',
-	// 数据库的账号
 	user:'newuser',
-	// 数据库密码
 	password:'123456',
-	// 当前连接的数据库名称
 	database:'crowdfunding_db',
-	// 端口号
 	port:'3306',
 	connectionLimit : 20,
 	waitForConnections: false
