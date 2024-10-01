b118e72f98612c8dd181a10dc0e95c70bbce32b2
Luchenchen1 <145833354+Luchenchen1@users.noreply.github.com>
2024-10-01 20:47:02 +0800

    Update client.js

--- routes/client.js
+++ routes/client.js
@@ -14,18 +14,18 @@ router.get('/search', function(req, res, next) {
 
 
 router.get('/fundraiser/:id', function(req, res, next) {
-  const fundraiserId = req.params.id; // 获取路径中的 ID
-  // 这里可以根据 ID 查询数据库，获取该筹款活动的详细信息
+  const fundraiserId = req.params.id; 
+
   res.render('./client/fundraiser_detail', { id: fundraiserId });
 });
 
 
 
-//底部公共
+
 router.get('/foot',function(req,res,next){
 	res.render('./client/footer')
 })
-//公共头部
+
 router.get('/head',function(req,res,next){
 	res.render('./client/header')
 })
